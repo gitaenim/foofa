565193f4cefc9b763de09ffc53ecab971e5659fe
Administrator <Administrator@304-12>
2022-12-12 16:37:28 +0900

    2022-12-12

---
+++ src/main/java/com/green/nowon/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.green.nowon.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests(authorize -> authorize
+					.antMatchers("/css/**").permitAll()
+					.antMatchers("/images/**","/js/**").permitAll()
+					.antMatchers("/","/signup").permitAll()
+					.antMatchers("/admin/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin(formLogin->formLogin
+					.loginPage("/login")
+					.loginProcessingUrl("/login")
+					.permitAll()
+			)
+			.csrf(csrf->csrf.disable())
+			;
+			
+		return http.build();
+	}
+}
