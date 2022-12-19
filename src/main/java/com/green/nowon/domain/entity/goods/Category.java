1fc77a76aee23d34a2da36e779ceacfc038f01cc
Administrator <Administrator@304-10>
2022-12-19 16:36:12 +0900

    20221219

--- src/main/java/com/green/nowon/domain/entity/goods/Category.java
+++ src/main/java/com/green/nowon/domain/entity/goods/Category.java
@@ -1,11 +1,11 @@
 package com.green.nowon.domain.entity.goods;
 
 public enum Category {
-	SOUP,
-	MEALKIT,
-	SIDEDISH,
-	SALTED,
-	BACONHAM,
+	SOUP, //국
+	MEALKIT, //밀키트
+	SIDEDISH,//반찬
+	SALTED,// 김치,장류
+	BACONHAM,//조리육
 	BEST,
 	SALE;
 }
