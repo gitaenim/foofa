7821f669db7deb8479658f79cc7de9c6e398f51b
hhan-a <ujb773@gmail.com>
2022-12-20 21:15:28 +0900

    Revert "비우기"
    This reverts commit c6537c89c5d4b125a0a7c6aee23fcce4b8193565.

---
+++ src/main/java/com/green/nowon/service/goodsservice/GoodsService.java
@@ -0,0 +1,20 @@
+package com.green.nowon.service.goodsservice;
+
+import java.util.Map;
+
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.green.nowon.domain.dto.GoodsInsertDTO;
+
+public interface GoodsService {
+
+	Map<String, String> fileTempUp(MultipartFile gimg);
+
+	void save(GoodsInsertDTO dto);
+	
+	void list(Model model);
+
+	void goodsOfCategory(long no, Model model);
+
+}
