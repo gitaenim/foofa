93031cc8bff976f871e74642611c3610e774d72d
Administrator <Administrator@304-12>
2022-12-22 17:16:36 +0900

    2022-12-22

--- src/main/java/com/green/nowon/controller/GoodsController.java
+++ src/main/java/com/green/nowon/controller/GoodsController.java
@@ -8,27 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.green.nowon.domain.entity.goods.GoodsListImg;
 import com.green.nowon.service.GoodsListImgService;
-import com.green.nowon.service.GoodsService;
+
 
 @Controller
 public class GoodsController {
 
-	@Autowired
-	GoodsService service;
 	
-	@Autowired
-	GoodsListImgService imgService;
-	
-	@GetMapping("/list")
-	public String list(Model model){
-		service.findAll(model);
-		imgService.findAll(model);
-		return "goods/list";
-	}
-	
-	@GetMapping("/common/category/{no}/goods")
-	public String goodsOfCategory(@PathVariable long no, Model model) {
-		service.goodsOfCategory(no, model);
-		return "goods/category-list";
-	}
 }
